783e814aaeab9eb4cf60b78386f900988cb837af
Josh Wardini <joshwardini@gmail.com>
2014-09-10 14:34:56 -0700

    Added command line interface

--- snippets.py
+++ snippets.py
@@ -1,5 +1,7 @@
 import logging
 import csv
+import argparse
+import sys
 
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
@@ -13,3 +15,20 @@ def put(name, snippet, filename):
         writer.writerow([name, snippet])
     logging.debug("Write sucessful")
     return name, snippet
+
+def make_parser():
+    """ Construct the command line parser """
+    logging.info("Constructing parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+
+    return parser
+
+def main():
+    """ Main function """
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
